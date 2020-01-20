b335a7670f0204705122fbb2fb5646079c79acfd
Ryan Albazzaz <haider3@pdx.edu>
2020-01-20 09:55:41 -0800

    Work on the test cases

--- airline/src/test/java/edu/pdx/cs410J/FlightTest.java
+++ airline/src/test/java/edu/pdx/cs410J/FlightTest.java
@@ -1,19 +1,27 @@
 package edu.pdx.cs410J;
 
 import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
 /**
  * Unit tests for the {@link Flight} class.
  */
+@RunWith(Suite.class)
 public class FlightTest {
   
   @Test(expected = UnsupportedOperationException.class)
   public void getArrivalStringNeedsToBeImplemented() {
-    //Flight flight = new Flight();
-    //flight.getArrivalString();
+    Flight flight = new Flight(1234,"12:03","12/01/2020","2:03","12//01/2020","pdx","lax");
+    flight.getArrivalString();
+    /*assertNotNull(flight.getArrivalString());*/
   }
 
   @Test
