337a89e049132a30b36f37d128e5e39749ddfcb1
Ryan Albazzaz <haider3@pdx.edu>
2020-01-21 23:57:38 -0800

    Added user flag checking and other refactoring to main class

--- airline/src/main/java/edu/pdx/cs410J/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/Project1.java
@@ -4,6 +4,9 @@ import java.io.*;
 import edu.pdx.cs410J.AbstractAirline;
 
 import java.sql.SQLSyntaxErrorException;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
@@ -14,22 +17,51 @@ import static java.lang.System.in;
  */
 public class Project1 {
 
-  public static void main(String[] args) {
+  @SuppressWarnings("Duplicates")
+    public static void main (String[] args){
     String airlineName, srcAirport, departTime, dstAirport, arriveTime, departDate, arriveDate;
-    int flightNumber;
+    int flightNumber ;
     String printOption, readmeOption;
 
-      try {
-        airlineName = args[0];
-        flightNumber = Integer.valueOf(args[1]);
-        srcAirport = args[2];
-        departTime = args[3];
-        departDate = args[4];
-        dstAirport = args[5];
-        arriveTime = args[6];
-        arriveDate = args[7];
-        printOption = args[8];
-        readmeOption = args[9];
+    try {
+
+          //To detect the flags.
+          int flag1 = 0;
+          int flag2  = 0;
+          for (String s: args) {
+            if (s.contains("-print"))
+              flag1 = 1;
+            if (s.contains("-README"))
+              flag2 = 1;
+          }
+
+          airlineName = args[0];
+          flightNumber = Integer.valueOf(args[1]);
+          srcAirport = args[2];
+          departTime = args[3];
+          departDate = args[4];
+          dstAirport = args[5];
+          arriveTime = args[6];
+          arriveDate = args[7];
+          if (flag1 != 1 && flag2 == 1){
+            readmeOption = args[8];
+            printOption = "";
+          }
+          else if (flag1 == 1 && flag2 == 1) {
+            printOption = args[8];
+            readmeOption = args[9];
+          }
+          else if (flag1 == 1 && flag2 != 1)
+          {
+            printOption = args[8];
+            readmeOption = "";
+          }
+
+      if (flag2 == 1){
+        System.out.println("README");
+        flag1 = 0;
+        exit(1);
+      }
 
         //To check If we have too many arguments.
         int i=0;
@@ -40,7 +72,6 @@ public class Project1 {
               exit(1);
             }
         }
-
 
         //To check the strings format.
         String dateTimeRetVal = StringChecker.DateTimeFormatChecker(arriveTime,arriveDate,departTime,departDate);
@@ -59,17 +90,14 @@ public class Project1 {
         Airline airline = new Airline(airlineName, new Flight(flightNumber, departTime,
                 departDate, arriveTime, arriveDate, srcAirport, dstAirport));
 
-        if (printOption.contains("-print")) {
+
+        if (flag1 == 1) {
           airline.getFlights();
         }
-
-        if (readmeOption.contains("README")) {
-            System.out.println("README Printed.");
-        }
-
+        
       } catch (Exception e) {
         if (e.getMessage().contains("out of bounds"))
-          System.out.println("Missing one or more of the Arguments. Program terminated.");
+          System.out.println("Missing command line arguments");
       }
 
    exit(1);
